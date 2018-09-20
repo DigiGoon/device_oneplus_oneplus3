89b5e04e62de4324aad18fd47fa034aba07d30d4
DigiGoon <kagatharatarang@gmail.com>
2018-09-20 19:45:15 +0000

    NFC renamed, inv uses cyanogenmod pocketmod

--- device.mk
+++ device.mk
@@ -348,12 +348,12 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     nfc_nci.msm8996 \
     NfcNci \
+    SecureElement \
     Tag \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+    android.hardware.nfc@1.1-service-nxp
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
